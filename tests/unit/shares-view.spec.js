7ad1e56bc95a0283f3da9fb1cf7c862bf94bac7e
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2023-01-06 15:53:49 +0100

    tests final

--- tests/unit/shares-view.spec.js
+++ tests/unit/shares-view.spec.js
@@ -1,7 +1,7 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import AllSharesView from '@/views/AllSharesView'
+
 import SharesUpdateForm from '@/components/SharesUpdateForm'
-import SharesCreateForm from '@/components/SharesCreateForm'
 
 describe('AllSharesView tests', () => {
   beforeEach(() => {
@@ -15,44 +15,8 @@ describe('AllSharesView tests', () => {
     })
     expect(wrapper.text()).toMatch(title)
   })
-
-  it('should return the correct header', () => {
-    const wrapper = mount(SharesCreateForm, {
-      propsData: {
-        shares: [
-          {
-            id: 1,
-            wkn: '1234',
-            name: 'Biontech',
-            stocksPrice: 100.5,
-            buy: true
-          }
-        ]
-      }
-    })
-    const cardTitle = wrapper.find('.card-title')
-    expect(cardTitle.text()).toBe('Biontech')
-  })
-
-  it('counts numbers of shares', () => {
-    fetch.mockResponseOnce(JSON.stringify(AllSharesView, {
-      propsData: {
-        shares: [
-          {
-            id: 1,
-            wkn: '1234',
-            name: 'Biontech',
-            stocksPrice: 100.5,
-            buy: true
-          }
-        ]
-      }
-    }))
+  it('should find update component', () => {
     const wrapper = mount(AllSharesView)
-    const cardTitle = wrapper.find('.card-title')
-    expect(cardTitle.text()).toBe('Max Mustermann')
-
-    const cards = wrapper.findAll('.col')
-    expect(cards.length).toBe(2)
+    expect(wrapper.findComponent(SharesUpdateForm)).toBeTruthy()
   })
 })
